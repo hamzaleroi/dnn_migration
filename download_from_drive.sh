b79d1042d450606d6f2f7c5efcc4c88b5bec0ff5
hamza <gh_benyamina@esi.dz>
2021-01-16 13:10:23 +0100

    "added the doawnloader"

--- download_from_drive.sh
+++ download_from_drive.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 # The file containing all the IDs from drive
 filename=$1
 while read line; do
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=$line" > /dev/null 
-code="$(awk '/_warning_/ { print $NF }' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=$line" -o screw_detection.tar
-tar -xvf screw_detection.tar
+    echo "downloading $line"
+    curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=$line" > /dev/null 
+    code="$(awk '/_warning_/ { print $NF }' /tmp/cookie)"
+    curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=$line" -o screw_detection.tar
+    tar -xvf screw_detection.tar
 done <  $filename
