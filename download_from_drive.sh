7ecc2d21cc6f8411935ab32629aa828d12ad5b83
hamza <gh_benyamina@esi.dz>
2021-01-16 13:37:02 +0100

    fixed the downloading script

--- download_from_drive.sh
+++ download_from_drive.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # The file containing all the IDs from drive
 filename=$1
+dir=$(pwd)
 while IFS=, read -r id name
 do
     echo "downloading $id $name"
     curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=$id" > /dev/null 
     code="$(awk '/_warning_/ { print $NF }' /tmp/cookie)"
     curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=$id" -o $name.tar
-    tar -xvf --strip-components=1 $name.tar
+    cd $name || ( mkdir $name && cd $name ) 
+    pwd
+    
+    tar -xvf  ../$name.tar --strip-components=1
 done <  $filename
