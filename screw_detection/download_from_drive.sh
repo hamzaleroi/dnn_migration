aaf7b586970c1c365ab1aa7af12277a370c5c345
hamza <gh_benyamina@esi.dz>
2021-01-16 12:06:14 +0100

    added data.py

--- screw_detection/download_from_drive.sh
+++
@@ -1,4 +0,0 @@
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1-6GulWdUGLBm76f9Y1IOFzg6h7vo1y06" > /dev/null 
-code="$(awk '/_warning_/ { print $NF }' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=1-6GulWdUGLBm76f9Y1IOFzg6h7vo1y06" -o screw_detection.zip
-unzip screw_detection.zip
