2f77d593fa8f529a82045a1b64855dd996ccfcd5
hamza <gh_benyamina@esi.dz>
2021-01-26 11:36:50 +0100

    added deletion

--- utils/upload_files.sh
+++ utils/upload_files.sh
@@ -1,4 +1,5 @@
 gcloud auth activate-service-account --key-file $1
 working_folder=$2
 bucket=$3
-gsutil cp -r $working_folder gs://$bucket
+gsutil rm -r * &&\
+	gsutil cp -r $working_folder gs://$bucket
