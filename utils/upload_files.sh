1bffd03decadd4c72a86dbd5c6a8310a2d893ba2
hamza <gh_benyamina@esi.dz>
2021-01-26 13:05:12 +0100

    last version

--- utils/upload_files.sh
+++ utils/upload_files.sh
@@ -1,5 +1,5 @@
-gcloud auth activate-service-account --key-file $1
+gcloud auth activate-service-account --key-file $1 || exit
 working_folder=$2
 bucket=$3
-gsutil rm -r * &&\
-	gsutil cp -r $working_folder gs://$bucket
+sudo gsutil rm -r gs://$bucket/*
+sudo gsutil cp -r $working_folder gs://$bucket
