26a9e0db09a342a24f26201d506815c537d486f0
Benyamina Hamza <43416089+hamzaleroi@users.noreply.github.com>
2021-01-27 11:36:32 +0100

    Added error message

--- utils/upload_files.sh
+++ utils/upload_files.sh
@@ -1,5 +1,5 @@
-gcloud auth activate-service-account --key-file $1
+gcloud auth activate-service-account --key-file $1 2> /dev/null || echo 'error, could not connect to service account'
 working_folder=$2
 bucket=$3
-sudo gsutil rm -r gs://$bucket/*
-sudo gsutil cp -r $working_folder gs://$bucket
+sudo gsutil rm -r gs://$bucket/* 2> /dev/null || echo 'error, the bucket may be empty'
+sudo gsutil cp -r $working_folder gs://$bucket 2> /dev/null || echo 'unable to copy files'
