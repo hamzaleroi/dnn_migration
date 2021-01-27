4d3f752fe7ca2e552a3fe3560fc37dd111808e32
Benyamina Hamza <43416089+hamzaleroi@users.noreply.github.com>
2021-01-27 11:31:59 +0100

    Update upload_files.sh

--- utils/upload_files.sh
+++ utils/upload_files.sh
@@ -1,5 +1,5 @@
 gcloud auth activate-service-account --key-file $1
 working_folder=$2
 bucket=$3
-gsutil rm -r gs://$bucket/*
-gsutil cp -r $working_folder gs://$bucket
+sudo gsutil rm -r gs://$bucket/*
+sudo gsutil cp -r $working_folder gs://$bucket
